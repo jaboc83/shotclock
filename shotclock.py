89e1301ef7aec6e6ab76fc779e6f5293f195ef91
Jake Moening <jmoening@unsignedbytes.com>
2016-03-10 12:18:35 -0600

    Refactor and add buzzer

--- shotclock.py
+++ shotclock.py
@@ -1,97 +1,152 @@
 from tkinter import *
 from tkinter import ttk
 import time
+import pygame.mixer
+from pygame.mixer import Sound
 
-toggle = True
-pause = True
-flashcount = 0
+# Is the timer paused?
+is_paused = True
+# 24 second shotclock
+max_clock = 24
+# Light color default is Red
+light = 'red'
+# Dark color default is Black
+dark = 'black'
+# How many seconds left before flipping colors
+time_running_out = 5
 
-def setColors():
-	if int(timer.get()) <= 5 :
-		lbl.configure(background='red')
-		root.configure(background='red')
-		lbl.configure(foreground='black')
-	else:
-		lbl.configure(background='black')
-		root.configure(background='black')
-		lbl.configure(foreground='red')
+# Use red background and black text
+def use_flipped_colors():
+  global dark, light
+  lbl.configure(background=light)
+  root.configure(background=light)
+  lbl.configure(foreground=dark)
 
+# Use black background and red text
+def use_normal_colors():
+  global dark, light
+  lbl.configure(background=dark)
+  root.configure(background=dark)
+  lbl.configure(foreground=light)
 
-def decrement_label():
-	global pause
-	if pause :
-		return
+# Set the clock colors based on how much time is left
+def set_clock_colors():
+  global time_running_out
+  # Invert the colors if it's the final seconds
+  if int(timer.get()) <= time_running_out:
+    use_flipped_colors()
+  else:
+    use_normal_colors()
 
-	timer.set(str(int(timer.get()) - 1).zfill(2))
+# Move the clock forward or back one second
+def adjust_clock(amount):
+  timer.set(str(int(timer.get()) + amount).zfill(2))
 
-	if timer.get() == "00":
-		flash()
-		return
+# Run the clock
+def countdown():
+  global is_paused, buzzer
+  one_second = 1000
 
-	setColors()
+  # Stop the clock
+  if is_paused :
+    return
 
-	root.after(1000, decrement_label)
+  # Decrement the timer 1 second
+  adjust_clock(-1)
 
-def flash():
-	global toggle
-	global flashcount
-	flashcount += 1
-	if toggle:	
-		root.configure(background='red')
-		lbl.configure(background='red')
-		lbl.configure(foreground='black')
-	else:
-		root.configure(background='black')
-		lbl.configure(background='black')
-		lbl.configure(foreground='red')
-	toggle = not toggle
-	if flashcount < 30:
-		root.after(100, flash)
-	else:
-		pause = True
-		flashcount = 0
-	
+  # Timer ran out
+  if int(timer.get()) == 0:
+    # Start flashing and pause the clock
+    flash(True)
+    is_paused = True
+    # Fire the buzzer
+    buzzer.play()
+    return
+
+  # Update the clock's colors
+  set_clock_colors()
+
+  # Wait 1 second then countdown again
+  root.after(one_second, countdown)
+
+# Flash the clock colors between normal and flipped
+def flash(use_fliped, flashcount):
+  flashcount += 1
+
+  if use_flipped:
+    use_flipped_colors()
+  else:
+    use_normal_colors()
+
+  # Only flash 30 times
+  if flashcount < 30:
+    # Flash to opposite colors after 100 ms
+    root.after(100, flash, not use_flipped, flashcount)
+
+# Reset the clock to max time and pause
 def reset(e):
-	timer.set("24")
+  global max_clock
+  global is_paused
+  is_paused = True
+  timer.set(str(max_clock))
 
-def pauseResume(e):
-	global pause
-	if pause:
-		pause = False
-		root.after(500, decrement_label)
-	else:
-		pause = True
+# toggle the clock running/paused state
+def toggle_clock(e):
+  global is_paused
+  if is_paused:
+    is_paused = False
+    root.after(500, countdown)
+  else:
+    is_paused = True
 
-def increment(e):
-	global pause
-	pause = True
-	timer.set(str(int(timer.get()) + 1).zfill(2))
-	setColors()
+# Set the clock back one second
+def increment_clock(e):
+  global is_paused
+  is_paused = True
+  # Don't increment beyond clock maximum
+  if int(timer.get()) < max_clock:
+    adjust_clock(1)
+  set_clock_colors()
 
-def decrement(e):
-	global pause
-	pause = True
-	if int(timer.get()) > 0:
-		timer.set(str(int(timer.get()) - 1).zfill(2))
-	setColors()
+# Move the clock forward one second
+def decrement_clock(e):
+  global is_paused
+  is_paused = True
+  # Don't decrement below 0
+  if int(timer.get()) > 0:
+    adjust_clock(-1)
+  set_clock_colors()
 
+# Initialize for sound
+pygame.mixer.init()
+# buzzer sound
+buzzer = Sound("./buzzer.mp3")
+
+# root UI element
 root = Tk()
-#w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-#root.geometry("%dx%d+0+0" % (w, h))
-root.title("Minnesota Rangers Shotclock")
-root.configure(background='black')
+root.title("UnsignedBytes Shotclock")
 root.attributes("-fullscreen", True)
 
-timer = StringVar() 
+# The clock's current time
+timer = StringVar()
 
-lbl = Label(root, textvariable=timer, width=2, height=1, fg="red", bg="black", font=("DS-Digital", 800))
-lbl.pack(side="top")
+# Label to shot current clock time in big red letters
+lbl = Label(root, textvariable=timer, width=2, height=1, font=("DS-Digital", 800))
+lbl.pack()
 
+# Reset the clock to max on Enter pressed
 root.bind("<Return>", reset);
-root.bind("<space>", pauseResume);
+# Pause/Resume the clock on Space pressed
+root.bind("<space>", toggle_clock);
+# Increment the clock on up arrow pressed
 root.bind("<Up>", increment);
+# Decrement the clock on down arrow pressed
 root.bind("<Down>", decrement);
 
+# Set the clock to Max initially
 reset(None)
-root.mainloop()
+# Start with the normal Color Scheme
+use_normal_colors()
 
+# Start the application
+root.mainloop()
